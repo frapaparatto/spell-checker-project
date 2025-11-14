936468f88619a9e18864f176a2afbf6b7abd972d
Francesco Paparatto <francescopaparatto@gmail.com>
2025-11-14 23:02:58 +0100

    started code profiling and ran experiment with and withoud length filtering. Stored results in 'results.txt' file

--- corrector.py
+++ corrector.py
@@ -1,16 +1,43 @@
 from typing import Dict, List
+from algorithms.damerau_levenshtein import damerau_levenshtein_distance
+
 
 def query_tokenizer(query: str) -> List[str]:
-    """Tokenize multi-word queries in single-word tokens."""
-    ...
+    """Tokenize multi-word queries in single-word tokens.
+    Separator: space.
+    """
+    return query.split()
+
 
 def calculate_probability(word: str, words: Dict[str, int]) -> float:
     """Calculate the probability of each single word in the dictionary."""
-    ...
 
-def suggest_correction(query: str, dictionary: Dict[str, int]) -> str:
+    return words[word] / sum(words.values())
+
+
+def suggest_correction(
+    query: str, dictionary: Dict[str, int], max_distance: int
+) -> str:
     """Main correction function."""
-    ...
 
+    if query in dictionary:
+        return query
 
+    candidates = []
+    for word in dictionary:
+        if abs(len(word)) - len(query) > max_distance:
+            continue
 
+        distance = damerau_levenshtein_distance(query, word)
+        if distance <= max_distance:
+            candidates.append((word, distance))
+
+    if not candidates:
+        return query
+
+    min_distance = min(distance for _, distance in candidates)
+    best_candidates = [
+        word for word, distance in candidates if distance == min_distance
+    ]
+
+    return max(best_candidates, key=lambda x: calculate_probability(x, dictionary))
--- main.py
+++ main.py
@@ -1,25 +1,41 @@
 import json
+import cProfile
+import pstats
 from utils import load_file, create_dictionary
+from corrector import suggest_correction, query_tokenizer
 
 
 def main():
-    # TODO: evaluate to create an helper function also for getting config files in order to handle JsonDecodeError 
+    # TODO: evaluate to create an helper function also for getting config files in order to handle JsonDecodeError
     with open("config.json", "r") as file:
         config = json.load(file)
 
-    try:
-        english_dictionary = create_dictionary(
-            load_file(config["dictionary"]["english_path"])
-        )
+    with cProfile.Profile() as profile:
+        try:
+            english_dictionary = create_dictionary(
+                load_file(config["dictionary"]["english_path"])
+            )
 
-        business_dictionary = create_dictionary(
-            load_file(config["dictionary"]["business_path"])
-        )
-    except FileNotFoundError as e:
-        print(e)
+            business_dictionary = create_dictionary(
+                load_file(config["dictionary"]["business_path"])
+            )
+        except FileNotFoundError as e:
+            print(e)
 
-    else:
-        dictionary = english_dictionary | business_dictionary
+        else:
+            dictionary = english_dictionary | business_dictionary
+            research_query = input("Search: ").strip().lower()
+
+            suggestions = [
+                suggest_correction(query, dictionary, max_distance=2)
+                for query in query_tokenizer(research_query)
+                if query
+            ]
+
+            print(" ".join(suggestions))
+
+    stats = pstats.Stats(profile).sort_stats(pstats.SortKey.TIME)
+    stats.print_stats("corrector|utils|algorithms|main")
 
 
 if __name__ == "__main__":
